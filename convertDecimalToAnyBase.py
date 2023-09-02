f8dd4bb60b00386c916ae08009b5b28fea1dbc67
Marcos Patrick <61922327+MarcosPatrickExe@users.noreply.github.com>
2023-09-02 20:26:15 -0300

    Add files via upload

---
+++ convertDecimalToAnyBase.py
@@ -0,0 +1,42 @@
+import math;
+
+print("digite um numero qualquer... ");
+numberI = int( input() );
+
+print("digite a base numérica para aplicar no numero anterior... ");
+baseI = int( input() );
+
+
+def recursive_DecimalToAnyBase(number, base, totalResult ):
+    
+    if(number < base):
+        finalResult = str(number)+""+ totalResult;
+        return finalResult;
+    
+    quotient = math.floor( number/base );
+    restOfDivision = number % base;
+    totalResult = str(restOfDivision) + totalResult;
+
+    return recursive_DecimalToAnyBase( quotient, base, totalResult );
+
+print("resultado: ", recursive_DecimalToAnyBase(numberI, baseI, "") );
+
+
+
+
+
+"""
+while True:
+    if(quotient < base ):
+        totalResult = str(quotient) + totalResult;
+        break;
+    else:
+        quotient = math.floor( number / base );
+        restOfDivision = number % base;
+
+        totalResult = str(restOfDivision) + totalResult;
+       
+        number = quotient; # na proxima iteracao o number (ou dividendo) vai ser o quociente dessa iteracao atual
+
+print("result: ", totalResult);
+"""
