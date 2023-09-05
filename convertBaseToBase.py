ba4f3e1bd90cf27b555811e238cb7fd046153c95
Marcos Patrick <61922327+MarcosPatrickExe@users.noreply.github.com>
2023-09-04 23:24:18 -0300

    Add files via upload

---
+++ convertBaseToBase.py
@@ -0,0 +1,51 @@
+import math;
+
+
+def recursive_AnyBaseToDecimal( currentIndex, cumulativeResult, number, base, exponent):
+   
+    if(currentIndex == -1 ):
+        return cumulativeResult;
+
+    cumulativeResult += int( number[currentIndex] ) * math.pow(base, exponent);
+    currentIndex -= 1;
+    exponent += 1;
+    return recursive_AnyBaseToDecimal( currentIndex, cumulativeResult, number, base, exponent)
+
+
+
+def recursive_DecimalToAnyBase(number, base, totalResult ):
+    
+    if(number < base):
+        finalResult = str(number)+""+ totalResult;
+        return finalResult;
+    
+    quotient = math.floor( number/base );
+    restOfDivision = number % base;
+    totalResult = str(restOfDivision) + totalResult;
+
+    return recursive_DecimalToAnyBase( quotient, base, totalResult );
+
+
+print("digite uma base numerica... ");
+base1 = int( input() );
+
+print("digite um numero que pertence a base numerica anterior... ");
+anyNumber = input();
+
+print("digite a base para a qual o numero sera convertido... ");
+base2 = int( input() );
+
+
+if( base1 < 10 ):
+    decimalResult = recursive_AnyBaseToDecimal( len(anyNumber)-1, 0, anyNumber, base1, 0);
+
+    baseDesiredResult = recursive_DecimalToAnyBase( decimalResult, base2, "");
+
+    print("o numero ",anyNumber,"(",base1,") na base ",base2," é ",baseDesiredResult);
+
+
+
+
+
+#from convertAnyBaseNumericalToDecimal import recursive_convertToDecimal;
+#from convertDecimalToAnyBase import recursive_Calc;
