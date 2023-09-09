b6e902c284fd67c8ab78605ffecfb8b161383dce
Marcos Patrick <61922327+MarcosPatrickExe@users.noreply.github.com>
2023-09-08 23:38:20 -0300

    Update convertAnyBaseNumericalToDecimal.py

--- convertAnyBaseNumericalToDecimal.py
+++ convertAnyBaseNumericalToDecimal.py
@@ -18,8 +18,9 @@ def recursive_AnyBaseToDecimal( currentIndex, cumulativeResult, number, base, ex
     currentIndex -= 1; # fazendo alguma alteracao na variavel antes de envia-la para a proxima recursao
     exponent += 1;
     return recursive_AnyBaseToDecimal( currentIndex, cumulativeResult, number, base, exponent)
+    
 
-
+# primeira chamada do método recursivo, passando o indice que aponta para o ultimo digito, resultado acumulado, numero à ser convertido, base numerica e expoente
 result = recursive_AnyBaseToDecimal( len(numberI)-1, 0, numberI, baseI, 0);
 print("resultado:  ", int(result) )
 
