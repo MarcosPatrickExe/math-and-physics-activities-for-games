1b3b2a4b8c669c4b313aed82e0db5a2be4e8f4d9
Marcos Patrick <61922327+MarcosPatrickExe@users.noreply.github.com>
2023-11-03 23:54:17 -0300

    Update Oblique_Throw_Physics.js

--- Oblique_Throw_Physics.js
+++ Oblique_Throw_Physics.js
@@ -48,3 +48,78 @@ corretamente aplicado
     circle(X, Y, 30);
     print(Y);
 }
+
+
+function lancamentoHorizontal(){
+  
+  if( Y <= height-20)
+      t += 0.1; // tempo
+  else
+      t=0;
+      
+  Yo = 0;     // posicao inicial
+  Vox = 30;   // velocidade inicial
+  g = 20;     // aceleracao da gravidade
+  X = 0;      // posicao final em X
+  Xo = 0;     // posicao inicial em X
+
+  // equacao do movimento constante:
+  // X = Xo + Vox * t
+  X = Xo + Vox * t;
+
+  // equacao de queda livre:
+  // Y = Yo - (g * t²) /2
+  Y = Yo + (g * t*t) / 2;
+
+  circle(X, Y, 30);
+}
+
+
+
+function lancamentoObliquo(){
+  
+   /* definicoes: 
+Sabendo da velocidade de lancamento na diagonal, 
+representada pelo vetor 'Vo', e fazendo relacoes com os vetores Vox e Voy, 
+temos:
+    
+   cos# = Vox / Vo, substituindo o Vox, temos:
+       =>  Vox = cos# * Vo
+   
+   sen# = Voy / Vo, substituindo o Voy, temos:
+       =>  Voy = sen# * Vo
+   
+   Agora temos as definicoes de Vx e Vy!  
+   OBS: # representa um angulo qualquer entre V e Vx
+  */
+  
+   // MRU para o eixo X horizontal:
+   // X = Xo + Vox * t  mas agora inserindo o valor de Vox:
+//    X = Xo + cos# * Vo *t
+  
+   // MRUV ou equacao da funcao da horaria da posicao:
+   // Y = Yo + Voy * t + (g*t²) /2   
+//    Y = Yo + sen# * Vo * t + (g*t²) /2
+  
+  
+  if( Y <= height-20)
+     t += 0.1;      // tempo
+  else
+     t = 0;         // reiniciando o lancamento atraves do tempo
+    
+  Vo = 160;         // vetor velocidade de lancamento (na diagonal)
+  angle = 77;       // angulo de lancamento do projetil
+  Yo = height-20;   // altura inicial
+  Vox = 30;         // velocidade inicial
+  g = 30;           // aceleracao da gravidade
+  Xo = 0;           // posicao inicial em X
+  
+  
+  // obtendo alcance total X, ou posicao final no eixo X
+  X = Xo + cos( angle*PI /180 ) * Vo * t;
+  
+  // obtendo a altura Y em funcao de t
+  Y = Yo - sin( angle*PI/180) * Vo *t + (g*t*t) / 2
+  
+  circle(X, Y, 30);
+}
